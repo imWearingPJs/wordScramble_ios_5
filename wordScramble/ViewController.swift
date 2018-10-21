30d3cf05c7f569446845ea5e6ed6ae22dfac7799
Michael Kozub <michaelkozub@gmail.com>
2018-10-21 10:58:34 -0600

    restarted

---
+++ wordScramble/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  wordScramble
+//
+//  Created by Michael Kozub on 10/21/18.
+//  Copyright © 2018 Michael Kozub. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         title = "Kozub"
+        
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } else {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+    
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+
+
+}
+
