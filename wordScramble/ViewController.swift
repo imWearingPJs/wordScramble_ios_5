3782b93ff434eb3acea2ac90fe30cfb80be2dc19
Michael Kozub <michaelkozub@gmail.com>
2018-10-21 09:49:31 -0600

    first commit

---
+++ wordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wordScramble
+//
+//  Created by Michael Kozub on 10/21/18.
+//  Copyright © 2018 Michael Kozub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
